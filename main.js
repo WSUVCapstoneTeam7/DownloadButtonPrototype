3673dd130b08c54ec7dc07f0e3bd231b414fd193
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-01-25 16:56:20 -0800

    creates download button template and begins vueJS component

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 
+Vue.component('edit-form',{
+    template: '#edit-form',
+    data(){
+        return {
+            posX: 100,
+            posY: 100,
+            rows: 5,
+            cols: 5,
+            showEditSeatingForm: false
+        }
+    },
+    methods:{},
+    created(){
+        // a bus listener for toggling visibility of the the edit seating form.
+        bus.$on('sigEditSeatFormOn', ()=>{
+            this.showEditSeatingForm = true;
+        })
+        bus.$on('sigEditSeatFormOff',()=>{
+            this.showEditSeatingForm = false;
+        })
+    }
+})
+
+
+var vm = new Vue({
+    el:'#vue-app',
+    data:{
+    },
+    methods:{}
+})
